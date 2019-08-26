2e5fe9390eab18cd305eb6f39903d3d1d6892390
Priyansh Trivedi <pc.priyansh@gmail.com>
2019-08-26 15:57:57 +0200

    something which works for simplequesitons

--- convert_auto.py
+++ convert_auto.py
@@ -5,82 +5,145 @@
 import sys
 import json
 import spacy
+import pickle
 import traceback
 from tqdm import tqdm
 sys.path.append('./auto')
 
 # Local/Custom imports
-from utilities import load_lcquad, InflectionError, save_auto
+from utilities import load_lcquad, InflectionError, save_auto, load_simplequestions
 from auto import rules
 
-# Important globals
-nlp = spacy.load("en_core_web_sm")
+
 RULES_PROFILE = 'all'
 
-with open("./info.json", 'r') as config_file:
-    config = json.load(config_file)
 
-# Load raw datasets
-lcquad = load_lcquad()
-simplequestions = None # @TODO
+def init(nlp: spacy, rules_dist: dict) -> dict:
 
-# Get all relevant rules and their distribution
-with open("./rules_dist.json", "r") as rules_file:
-    rules_dists = json.load(rules_file)
+    # Initialize the rule classes and put it in the dictionary as well.
+    for rule_name, rule_prob in rules_dist['distribution'].items():
+        # Pull out the rule implementation
+        rule_obj = getattr(rules, rule_name)(nlp)
+        rules_dist['distribution'][rule_name] = (rule_prob, rule_obj)
 
-# Use "all" rules for now
-rules_dist = [x for x in rules_dists if x['_profile'] == RULES_PROFILE][0]
-
-# Initialize the rule classes and put it in the dictionary as well.
-for rule_name, rule_prob in rules_dist['distribution'].items():
-    # Pull out the rule implementation
-    rule_obj = getattr(rules, rule_name)(nlp)
-    rules_dist['distribution'][rule_name] = (rule_prob, rule_obj)
-
-# Add configuration and other metadata to file
-lcquad['_config'] = config
-lcquad['_rule_application_distribution'] = [x for x in rules_dists if x['_profile'] == RULES_PROFILE][0]
-
-'''
-    Now loop over the dataset(s) and inflect it.
-'''
-# LC-QuAD inflections
-for i, datum in tqdm(enumerate(lcquad['raw']['train']), total=len(lcquad['raw']['train'])):
-    raw, inflected = datum['corrected_question'], []
-    for rule_name, (rule_prob, rule_obj) in rules_dist['distribution'].items():
-        # Make a doc of input
-        raw_spacied = nlp(raw)
-        try:
-            inflected.append([rule_name, rule_obj.apply(raw_spacied, rule_prob)])
-        except TypeError:
-            traceback.print_exc()
-            print(raw)
-            raise InflectionError
-
-    lcquad['auto'].setdefault('train', []).append(inflected)
-
-# LC-QuAD inflections
-for i, datum in tqdm(enumerate(lcquad['raw']['test']), total=len(lcquad['raw']['test'])):
-    raw, inflected = datum['corrected_question'], []
-    for rule_name, (rule_prob, rule_obj) in rules_dist['distribution'].items():
-        # Make a doc of input
-        raw_spacied = nlp(raw)
-        try:
-            inflected.append([rule_name, rule_obj.apply(raw_spacied, rule_prob)])
-        except TypeError:
-            traceback.print_exc()
-            print(raw)
-            raise InflectionError
-
-    lcquad['auto'].setdefault('test', []).append(inflected)
-
-# Dump this in the `data/auto` dir
-save_auto('lcquad', lcquad)
-
-print(lcquad.keys())
+    return rules_dist
 
 
+def inflect_lcquad(data: dict, rules_dist: dict) -> dict:
+    """Simply loop over the raw, and inflect it."""
 
+    # LC-QuAD inflections
+    for i, datum in tqdm(enumerate(data['raw']['train']), total=len(data['raw']['train'])):
+        raw, inflected = datum['corrected_question'], []
+        for rule_name, (rule_prob, rule_obj) in rules_dist['distribution'].items():
+            # Make a doc of input
+            raw_spacied = nlp(raw)
+            try:
+                flag, res = rule_obj.apply(raw_spacied, rule_prob)
+                res = str(res)
+                inflected.append([rule_name, (flag, res)])
+            except TypeError:
+                traceback.print_exc()
+                print(raw)
+                raise InflectionError
+
+        data['auto'].setdefault('train', []).append(inflected)
+
+    # LC-QuAD inflections
+    for i, datum in tqdm(enumerate(data['raw']['test']), total=len(data['raw']['test'])):
+        raw, inflected = datum['corrected_question'], []
+        for rule_name, (rule_prob, rule_obj) in rules_dist['distribution'].items():
+            # Make a doc of input
+            raw_spacied = nlp(raw)
+            try:
+                flag, res = rule_obj.apply(raw_spacied, rule_prob)
+                res = str(res)
+                inflected.append([rule_name, (flag, res)])
+            except TypeError:
+                traceback.print_exc()
+                print(raw)
+                raise InflectionError
+
+        data['auto'].setdefault('test', []).append(inflected)
+
+    return data
+
+
+def inflect_simplequestions(data: dict, rules_dist: dict) -> dict:
+    """ Similar logic as above """
+
+    for i, datum in tqdm(enumerate(data['raw']['train']), total=len(data['raw']['train'])):
+        uid, raw = datum.values()
+        inflected = []
+
+        for rule_name, (rule_prob, rule_obj) in rules_dist['distribution'].items():
+            raw_spacied = nlp(raw)
+            try:
+                flag, res = rule_obj.apply(raw_spacied, rule_prob)
+                res = str(res)
+                inflected.append([uid, rule_name, (flag, res)])
+            except TypeError:
+                traceback.print_exc()
+                print(raw)
+                raise InflectionError
+
+        data['auto'].setdefault('train', []).append(inflected)
+
+    return data
+
+
+def inflect_again(data: dict) -> dict:
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+
+    toinflect = ['simplequestions']
+
+    # Important globals
+    nlp = spacy.load("en_core_web_sm")
+
+    with open("./info.json", 'r') as config_file:
+        config = json.load(config_file)
+
+    # Load raw datasets
+    lcquad = load_lcquad()
+    simplequestions = load_simplequestions()
+
+    # Get all relevant rules and their distribution
+    with open("./rules_dist.json", "r") as rules_file:
+        rules_dists = json.load(rules_file)
+
+    # Add configuration and other metadata to file
+    lcquad['_config'] = config
+    lcquad['_rule_application_distribution'] = RULES_PROFILE
+    simplequestions['_rule_application_distribution'] = RULES_PROFILE
+
+    # Use "all" rules for now
+    rules_dist = [x for x in rules_dists if x['_profile'] == RULES_PROFILE][0]
+    rules_dist = init(nlp, rules_dist)
+
+    if 'lcquad' in toinflect:
+        # Get inflections
+        lcquad = inflect_lcquad(lcquad, rules_dist)
+
+        # TO DELETE
+        with open('tmp.pkl', 'wb+') as f:
+            pickle.dump(lcquad, f)
+
+        save_auto('lcquad', lcquad)
+
+    if 'simplequestions' in toinflect:
+        # Get inflections
+        simplequestions = inflect_simplequestions(simplequestions, rules_dist)
+
+        # TO DELETE
+        with open('tmp_sq.pkl', 'wb+') as f:
+            pickle.dump(simplequestions, f)
+
+        save_auto('simplequestions', simplequestions)
+
+    print("Done")
 
 
 
--- utilities.py
+++ utilities.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import pandas as pd
 import json
 
 DATA_DIR = Path('./data')
@@ -30,18 +31,49 @@ def load_lcquad():
     return lcquad
 
 
+def load_simplequestions():
+    """ Assume we only have train data """
+    simplequestions = {
+        "rawdir": DATA_DIR / "raw" / "simplequestions",
+        "autodir": DATA_DIR / "auto" / "simplequestions",
+        "golddir": DATA_DIR / "gold" / "simplequestions",
+        "raw": {},
+        "auto": {}
+    }
+
+    data = pd.read_csv(simplequestions['rawdir'] / 'amt.csv').values.tolist()
+
+    real_data = []
+    for datum in data:
+        assert len(datum) is 4
+        _datum = [{
+            'uid': datum[0],
+            'question': datum[1]
+        }, {
+            'uid': datum[2],
+            'question': datum[3]
+        }]
+        real_data += _datum
+
+    # Assume its all train data
+    simplequestions['raw']['train'] = real_data
+    return simplequestions
+
+
 def save_auto(dataset: str, data: dict) -> None:
     """ Dumping inflected data """
 
     assert dataset in DATASETS, f"Unknown Dataset"
-    dump_dir = AUTO_DIR / dataset
-    with open(dump_dir / 'dump.json') as f:
+    dump_dir = data['autodir']
+
+    # Convert all paths to str
+    for k, v in data.items():
+        if type(v) is type(Path()):
+            data[k] = str(v)
+
+    # Dump eeet
+    with open(dump_dir / 'dump.json', 'w+') as f:
         json.dump(data, f)
-
-
-def load_simplequestions():
-    # @TODO: this
-    raise NotImplementedError
 
 
 if __name__ == '__main__':
